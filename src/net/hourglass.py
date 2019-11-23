9e75e178f4d57094dc0e53d797d24876632bd6d6
demetermart <demetermart@gmail.com>
2019-11-23 15:49:42 +0100

    annot

--- src/net/hourglass.py
+++ src/net/hourglass.py
@@ -42,7 +42,7 @@ class HourglassNet(object):
                                     inres=self.inres, outres=self.outres, is_train=True)
         train_gen = train_dataset.generator(batch_size, self.num_stacks, sigma=1, is_shuffle=True,
                                             rot_flag=True, scale_flag=True, flip_flag=True)
-
+        
         csvlogger = CSVLogger(
             os.path.join(model_path, "csv_train_" + str(datetime.datetime.now().strftime('%H:%M')) + ".csv"))
         modelfile = os.path.join(model_path, 'weights_{epoch:02d}_{loss:.2f}.hdf5')
