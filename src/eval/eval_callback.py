73e8a80db0bb31fb173e1abbc86af2c49174b42d
demetermart <demetermart@gmail.com>
2019-11-26 12:58:33 +0100

    no message

--- src/eval/eval_callback.py
+++ src/eval/eval_callback.py
@@ -44,7 +44,7 @@ class EvalCallBack(keras.callbacks.Callback):
 
         acc = total_suc * 1.0 / (total_fail + total_suc)
         
-        print('all', total_fail, total_suc))
+        print('all', total_fail, total_suc)
         print('mean diff', np.mean(mss))
         print 'Eval Accuray ', acc, '@ Epoch ', epoch
 
